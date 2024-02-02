83ae6c9d52cb41526791067a92d28066c3ad5ec4
mlee51 <mleeteak13@gmail.com>
2024-02-01 22:15:37 -0500

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,39 @@
 import Image from "next/image";
+import a from '../public/1.jpeg'
+import b from '../public/2.jpg'
+import c from '../public/3.jpg'
+import d from '../public/4.jpg'
+import e from '../public/5.jpg'
+import f from '../public/6.jpg'
+import g from '../public/7.jpg'
+import h from '../public/8.jpg'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <>
+      <h1 className="font-bold text-gray-100 text-6xl p-6">Michael Lee 3D Portfolio</h1>
+      <div className="flex flex-wrap w-full justify-center [&>*]:max-w-96 [&>*]:p-2">
+        <div><Image className="rounded-lg" src={a} alt="a" /></div>
+        <div><Image className="rounded-lg" src={b} alt="b" /></div>
+        <div><Image className="rounded-lg" src={c} alt="c" /></div>
+        <div><Image className="rounded-lg" src={d} alt="d" /></div>
+        <div><Image className="rounded-lg" src={e} alt="e" /></div>
+        <div><Image className="rounded-lg" src={f} alt="f" /></div>
+        <div><Image className="rounded-lg" src={g} alt="g" /></div>
+        <div><Image className="rounded-lg" src={h} alt="h" /></div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="flex flex-wrap w-full justify-center [&>*]:p-2 [&>*]:min-w-96 mb-2">
+        <div className="aspect-video"><iframe className="h-full w-full" width="100%" src="https://www.youtube.com/embed/WUZVEckYZAU?si=LangxwRe1sLXqFK5" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe></div>
+        <div className="aspect-video"><iframe className="h-full w-full" width="100%" src="https://player.vimeo.com/video/444084452?h=2e7fbbeb15" allow="autoplay; fullscreen; picture-in-picture" allowFullScreen></iframe></div>
+        <div className="aspect-video"><iframe className="h-full w-full" width="100%" src="https://player.vimeo.com/video/414935057?h=5b8896b28d" allow="autoplay; fullscreen; picture-in-picture" allowFullScreen></iframe></div>
+        <div className="aspect-video"><iframe className="h-full w-full" width="100%" src="https://player.vimeo.com/video/346878279?h=58dc3773f6" allow="autoplay; fullscreen; picture-in-picture" allowFullScreen></iframe></div>
+        <div className="aspect-video"><iframe className="h-full w-full" width="100%" src="https://player.vimeo.com/video/328047394?h=398b0504e9" allow="autoplay; fullscreen; picture-in-picture" allowFullScreen></iframe></div>
+        <div className="aspect-video"><iframe className="h-full w-full" width="100%" src="https://www.youtube.com/embed/4yEYPrYMU14?si=8L3r2qJqHd7HF3PJ" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe></div>
+        <div className="aspect-video"><iframe className="h-full w-full" width="100%" src="https://www.youtube.com/embed/EEvuVo8wGhU?si=tkdU-WduY6YcDwfE" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe></div>
+        <div className="aspect-video"><iframe className="h-full w-full" width="100%" src="https://www.youtube.com/embed/PmK9b9r1Bno?si=O26nvlnVWjfPIycU" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe></div>
+        <div className="aspect-video"><iframe className="h-full w-full" width="100%" src="https://www.youtube.com/embed/SE3zVO6fwu0" allowFullScreen></iframe></div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <br/>
+    </>
   );
 }
